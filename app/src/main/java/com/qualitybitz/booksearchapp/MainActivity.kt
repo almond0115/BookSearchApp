38e5f0ceaf8158b912a35d9e3138a81914564045
almond0115 <almond0115@users.noreply.github.com>
2023-02-03 00:09:11 +0900

    Practice2A-앱 초기구조 작성하기 (View Binding, BottomNavigationVIew)

--- app/src/main/java/com/qualitybitz/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/qualitybitz/booksearchapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.qualitybitz.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.qualitybitz.booksearchapp.databinding.ActivityMainBinding
+import com.qualitybitz.booksearchapp.view.FavoriteFragment
+import com.qualitybitz.booksearchapp.view.SearchFragment
+import com.qualitybitz.booksearchapp.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // 앱이 처음으로 실행되었을 때에만 화면에 SearchFragmentView를 띄움
+        setupBottomNavigationView()
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
